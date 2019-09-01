af2c381ee2d3fe6d212e5c8456a98d8534e5df0f
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-09-01 17:12:17 +0200

    steps recursive

--- steps/steps.js
+++ steps/steps.js
@@ -1,15 +1,38 @@
-const steps = num => {
-    let it = 0;
-    do {
-        let temp = ''
-        for(let yt=0; yt<num; yt++){
-            if(yt<=it)
-                temp += '#'
-                else
-                temp += ' '
-        }
-        console.log(temp)
-        it++
+// Recursive solution 
+const steps = (num, row = 0, tempString='') => {
+    if (row === num)
+    {
+        console.log(tempString)
+        return;
     }
-    while (it<num)
+
+    if(tempString.length===num){
+        console.log(tempString)
+        return steps(num,row + 1)
+    }
+
+    if(tempString.length<=row)
+    tempString+='#'
+    else
+    tempString+=' '
+
+    steps(num, row, tempString)
 }
+
+
+// const steps = num => {
+//     let it = 0;
+//     do {
+//         let temp = ''
+//         for(let yt=0; yt<num; yt++){
+//             if(yt<=it)
+//                 temp += '#'
+//                 else
+//                 temp += ' '
+//         }
+
+//         console.log(temp)
+//         it++
+//     }
+//     while (it<num)
+// }
